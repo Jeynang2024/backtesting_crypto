e7acc70d632b1dcae27acbb2df389bb0a59e56ca
Jey Nang Gogoi <156996004+Jeynang2024@users.noreply.github.com>
2025-01-31 15:57:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import backtrader as bt
+import numpy as np
+from strategy import *
+import pandas as pd
+from his_data import *
+from binance.client import Client
+import config
+
+
+
+client = Client(config.API_KEY,config.SECRET_KEY)
+
+
+
+def run_backtest(strategy_class,strategy_params,symbol,initial_cash,start,end,interval):
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(strategy_class,**strategy_params)
+    data = fetch_historical_data(symbol,client,start,end,interval)
+    data_feed =bt.feeds.GenericCSVData(
+        dataname=data,
+            datetime=0,
+            open=1,
+            high=2,
+            low=3,
+            close=4,
+            volume=5,
+            openinterest=-1,
+            dtformat=('%Y-%m-%d %H:%M:%S') 
+    )
+    cerebro.adddata(data_feed,name=symbol)
+    cerebro.broker.set_cash(initial_cash)
+    cerebro.run()
+    return cerebro
+
